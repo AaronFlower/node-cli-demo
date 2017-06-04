b74d080c13c0d144d306f46db5a46a327ea3b9fa
AaronFlower <haojunzhan@gmail.com>
2017-06-04 23:26:42 +0800

    1. add commander package to improve aaron-cli

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
-console.log('Hello world, node cli demo')
-console.log('use npm link when developing your npm projet')
+var program = require('commander')
+program
+	.arguments('<file>')
+	.option('-u, --username <username>', 'The user to authenticate to')
+	.option('-p, --password <password>', 'The user\'s password')
+	.action(function (file) {
+		console.log('user: %s, password: %s, file: %s', program.username, program.password, file)
+	})
+	.parse(process.argv)
